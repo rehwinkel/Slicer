b2fc177863fc24b88324848f090b8572b128cf5a
Ian Rehwinkel <ian.rehwinkel@kabelmail.de>
2019-03-26 17:18:32 +0100

    Restructured code in geometry classes.

--- src/math/Ray.kt
+++ src/math/Ray.kt
@@ -2,4 +2,12 @@ package math
 
 data class Ray(val positionVector: Vector3, val directionVector: Vector3) {
 
+    fun planeIntersection(plane: Plane): Vector3? {
+        if (FloatUtil.floatNE(plane.normalVector.dot(directionVector), 0f)) {
+            val result = plane.normalVector.dot(positionVector - positionVector) / plane.normalVector.dot(directionVector)
+            return directionVector * result + positionVector
+        }
+        return null
+    }
+
 }
--- src/math/Triangle.kt
+++ src/math/Triangle.kt
@@ -13,11 +13,44 @@ data class Triangle(val v1: Vector3, val v2: Vector3, val v3: Vector3, val norma
     val vertices: Collection<Vector3>
         get() = listOf(v1, v2, v3)
 
-    val rays: Collection<Ray>
-        get() = listOf(
-                Ray(v2, (v1 - v2).normalized()),
-                Ray(v3, (v1 - v3).normalized()),
-                Ray(v3, (v3 - v2).normalized())
-        )
+    fun planeIntersection(plane: Plane): Pair<Vector3, Vector3>? {
+        when (this.vertices.count { plane.pointOnPlane(it) }) {
+            1 -> {
+                val verts = ArrayList(this.vertices)
+                val p1 = verts.removeAt(verts.indexOfFirst { plane.pointOnPlane(it) })
+
+                val root: Vector3
+                if (verts.count { plane.pointAbovePlane(it) } == 1) {
+                    root = verts.removeAt(verts.indexOfFirst { plane.pointAbovePlane(it) })
+                } else if (verts.count { plane.pointBelowPlane(it) } == 1) {
+                    root = verts.removeAt(verts.indexOfFirst { plane.pointBelowPlane(it) })
+                } else {
+                    return null
+                }
+
+                val r1 = Ray(root, (root - verts.removeAt(0)).normalized())
+                val p2 = r1.planeIntersection(plane)
+                return p2?.let { Pair(p1, p2) }
+            }
+            0 -> {
+                val verts = ArrayList(this.vertices)
+                val rootIndex = if (verts.count { plane.pointAbovePlane(it) } == 1) {
+                    verts.indexOfFirst { plane.pointAbovePlane(it) }
+                } else if (verts.count { plane.pointBelowPlane(it) } == 1) {
+                    verts.indexOfFirst { plane.pointBelowPlane(it) }
+                } else {
+                    return null
+                }
+
+                val root = verts.removeAt(rootIndex)
+                val r1 = Ray(root, (root - verts.removeAt(0)).normalized())
+                val r2 = Ray(root, (root - verts.removeAt(0)).normalized())
+                val p1 = r1.planeIntersection(plane)
+                val p2 = r2.planeIntersection(plane)
+                return p1?.let { p2?.let { Pair(p1, p2) } }
+            }
+            else -> return null
+        }
+    }
 
 }
